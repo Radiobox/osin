11e30e8130f63b5b0f37905f71dc69feed478de8
Samuel Nelson <valczir.darkvein@gmail.com>
2014-04-28 10:05:37 -0600

    Use HttpError values in all util.go functions

--- util.go
+++ util.go
@@ -16,30 +16,40 @@ type BasicAuth struct {
 
 // CheckBasicAuth reads Basic authorization from the Authorization
 // header.
-func CheckBasicAuth(r *http.Request) (*BasicAuth, error) {
+func CheckBasicAuth(r *http.Request) (*BasicAuth, *HttpError) {
 	if r.Header.Get("Authorization") == "" {
 		return nil, nil
 	}
 
 	s := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 	if len(s) != 2 || s[0] != "Basic" {
-		return nil, errors.New("Invalid authorization header")
+		return nil, &HttpError{
+			Status: http.StatusBadRequest,
+			Message: "Invalid authorization header",
+		}
 	}
 
 	b, err := base64.StdEncoding.DecodeString(s[1])
 	if err != nil {
-		return nil, err
+		return nil, &HttpError{
+			Status: http.StatusBadRequest,
+			Message: "Could not decode basic auth: " + err.Error(),
+		}
 	}
 	pair := strings.SplitN(string(b), ":", 2)
 	if len(pair) != 2 {
-		return nil, errors.New("Invalid authorization message")
+		return nil, &HttpError{
+			Status: http.StatusBadRequest,
+			Message: "Basic authorization must be a base64-encoded id:secret pair",
+		}
 	}
 
 	return &BasicAuth{Username: pair[0], Password: pair[1]}, nil
 }
 
-// Check client authentication in params if allowed, and on authorization header
-func CheckClientAuth(r *http.Request, params objx.Map, useparams bool) (*BasicAuth, error) {
+// ChecClientAuth checks for client_id and client_secret in the
+// Authorization header and (if useparams is true) request parameters.
+func CheckClientAuth(r *http.Request, params objx.Map, useparams bool) (*BasicAuth, *HttpError) {
 	if useparams {
 		ret := &BasicAuth{
 			Username: params.Get("client_id").Str(),
@@ -54,17 +64,17 @@ func CheckClientAuth(r *http.Request, params objx.Map, useparams bool) (*BasicAu
 }
 
 // GetValidAuth loads authorization using CheckClientAuth, then
-// ensures that the authorization is valid.  It writes any error
-// messages to the passed in *Response value and returns the error (so
-// that you can stop processing if you don't need to go any further).
-func GetValidAuth(request *http.Request, params objx.Map, allowSecretInParams bool, writer *Response) (*BasicAuth, error) {
+// ensures that the authorization is valid.  It returns a *HttpError
+// if there are any problems with the request.
+func GetValidAuth(request *http.Request, params objx.Map, allowSecretInParams bool) (*BasicAuth, *HttpError) {
 	auth, err := CheckClientAuth(request, params, allowSecretInParams)
 	if err != nil || auth == nil {
-		writer.SetError(E_INVALID_REQUEST, "")
 		if err == nil {
-			err = errors.New("Client authentication not sent")
+			err = &HttpError{
+				Status: http.StatusUnauthorized,
+				Message: "No client credentials received",
+			}
 		}
-		writer.InternalError = err
 		return nil, err
 	}
 	return auth, nil
@@ -72,19 +82,22 @@ func GetValidAuth(request *http.Request, params objx.Map, allowSecretInParams bo
 
 // GetValidClient takes a client id and a *Response, then
 // tries to load a client from storage and validate that client.  It
-// will return nil for the returned Client and an error if there are
-// any problems locating or validating the requested client (i.e. if
-// the client doesn't exist or has an empty GetRedirectUri()
+// will return nil for the returned Client and a *HttpError if there
+// are any problems locating or validating the requested client (i.e.
+// if the client doesn't exist or has an empty GetRedirectUri()
 // response), or the validated Client and nil for an error otherwise.
-func (s *Server) GetValidClient(id string, writer *Response) (Client, error) {
+func (s *Server) GetValidClient(id string) (Client, *HttpError) {
 	client, err := s.Storage.GetClient(id)
 	if err != nil {
-		writer.SetError(E_SERVER_ERROR, "")
-		writer.InternalError = err
-		return nil, err
+		if httpErr, ok := err.(*HttpError); ok {
+			return nil, httpErr
+		}
+		return nil, &HttpError{
+			Status: http.StatusInternalServerError,
+			Message: err.Error(),
+		}
 	}
 	if client == nil || client.GetRedirectUri() == "" {
-		writer.SetError(E_UNAUTHORIZED_CLIENT, "")
 		return nil, deferror.Get(E_UNAUTHORIZED_CLIENT)
 	}
 	return client, nil
@@ -93,13 +106,12 @@ func (s *Server) GetValidClient(id string, writer *Response) (Client, error) {
 // GetValidClientWithSecret takes a client id, secret, and a
 // *Response, then returns the client if both GetValidClient returns a
 // valid client and the passed in secret matches the client's secret.
-func (s *Server) GetValidClientWithSecret(id, secret string, writer *Response) (Client, error) {
+func (s *Server) GetValidClientWithSecret(id, secret string) (Client, *HttpError) {
 	client, err := s.GetValidClient(id, writer)
 	if err != nil {
 		return nil, err
 	}
 	if client.GetSecret() != secret {
-		writer.SetError(E_UNAUTHORIZED_CLIENT, "")
 		return nil, deferror.Get(E_UNAUTHORIZED_CLIENT)
 	}
 	return client, nil
@@ -112,19 +124,21 @@ func (s *Server) GetValidClientWithSecret(id, secret string, writer *Response) (
 // (i.e. if the AuthorizeData's Client value returned from GetClient()
 // is nil or has an empty GetRedirectUri() response), or the validated
 // AuthorizeData and nil for an error otherwise.
-func (s *Server) GetValidAuthData(code string, writer *Response) (AuthorizeData, error) {
+func (s *Server) GetValidAuthData(code string) (AuthorizeData, *HttpError) {
 	authData, err := s.Storage.LoadAuthorize(code)
 	if err != nil {
-		writer.SetError(E_INVALID_GRANT, "")
-		writer.InternalError = err
-		return nil, err
+		if httpErr, ok := err.(*HttpError); ok {
+			return nil, httpErr
+		}
+		return nil, &HttpError{
+			Status: http.StatusInternalServerError,
+			Message: err.Error(),
+		}
 	}
 	if authData.GetClient() == nil || authData.GetClient().GetRedirectUri() == "" {
-		writer.SetError(E_UNAUTHORIZED_CLIENT, "")
 		return nil, deferror.Get(E_UNAUTHORIZED_CLIENT)
 	}
 	if authData.IsExpired() {
-		writer.SetError(E_INVALID_GRANT, "")
 		return nil, deferror.Get(E_INVALID_GRANT)
 	}
 	return authData, nil
@@ -137,20 +151,22 @@ func (s *Server) GetValidAuthData(code string, writer *Response) (AuthorizeData,
 // (i.e. if the AccessData's Client value returned from GetClient()
 // is nil or has an empty GetRedirectUri() response), or the validated
 // AccessData and nil for an error otherwise.
-func (s *Server) GetValidAccessData(token string, writer *Response) (AccessData, error) {
+func (s *Server) GetValidAccessData(token string) (AccessData, *HttpError) {
 	access, err := s.Storage.LoadAccess(token)
 	if err != nil {
-		writer.SetError(E_INVALID_REQUEST, "")
-		writer.InternalError = err
-		return nil, err
+		if httpErr, ok := err.(*HttpError); ok {
+			return nil, httpErr
+		}
+		return nil, &HttpError{
+			Status: http.StatusInternalServerError,
+			Message: err.Error(),
+		}
 	}
 	if access.GetClient() == nil {
-		writer.SetError(E_UNAUTHORIZED_CLIENT, "")
-		return nil, err
+		return nil, deferror.Get(E_UNAUTHORIZED_CLIENT)
 	}
 	if access.IsExpired() {
-		writer.SetError(E_INVALID_GRANT, "")
-		return nil, err
+		return nil, deferror.Get(E_INVALID_GRANT)
 	}
 	return access, nil
 }
@@ -162,15 +178,18 @@ func (s *Server) GetValidAccessData(token string, writer *Response) (AccessData,
 // (i.e. if the AccessData's Client value returned from GetClient()
 // is nil or has an empty GetRedirectUri() response), or the validated
 // AccessData and nil for an error otherwise.
-func (s *Server) GetValidRefresh(token string, writer *Response) (AccessData, error) {
+func (s *Server) GetValidRefresh(token string) (AccessData, error) {
 	access, err := s.Storage.LoadRefresh(token)
 	if err != nil {
-		writer.SetError(E_INVALID_GRANT, "")
-		writer.InternalError = err
-		return nil, err
+		if httpErr, ok := err.(*HttpError); ok {
+			return nil, httpErr
+		}
+		return nil, &HttpError{
+			Status: http.StatusInternalServerError,
+			Message: err.Error(),
+		}
 	}
 	if access.GetClient() == nil {
-		writer.SetError(E_UNAUTHORIZED_CLIENT, "")
 		return nil, deferror.Get(E_UNAUTHORIZED_CLIENT)
 	}
 	return access, nil
